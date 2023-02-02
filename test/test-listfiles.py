a41ebbc051f84b5345321c1b98af70ee58012a0d
WinserverSupport <64587872+WinserverSupport@users.noreply.github.com>
2023-02-01 19:23:05 -0500

    Updated test-listfiles.py from github

--- test/test-listfiles.py
+++ test/test-listfiles.py
@@ -1,7 +1,7 @@
-import os
-#import ctypes
-#from ctypes import wintypes
+# Example of using wcPython API (wcPAPI)
+# List Files
 
+import os
 import wcpapi
 from wcpapi import wcserver_h, wcsmw_h
 from wcpapi.wcserver_h import *
@@ -10,13 +10,21 @@ from wcpapi.wcsmw_h import *
 
 print("* List Files by Area/Name")
 
-WildcatServerConnect(0)
-WildcatServerCreateContext()
-LoginSystem()
-
+if not WildcatServerConnect(0)):
+   exit(1)
+if not WildcatServerCreateContext():
+   print(f"Error Creating Context with WildcatServerCreateContext()")
+   exit(1)
+if not LoginSystem():
+   print(f"Error LoginSystem()")
+   exit(1)
+ 
 frec = TFileRecord()
 tid  =  DWORD()
 
+# Since frec is cleared and tid is cleared (all zero), the first record 
+# will be returned with GetNextFileRec().  If you set the frec.Area, then
+# that is the conference it will begin.
 while GetNextFileRec(FileAreaNameKey, frec, tid):
    print("* Area: {} | {} | {}".format(frec.Area, frec.SFName.ljust(15), frec.Name))
 
